4fc9f9cb24e60e01eed396f7a38d709c669d04c8
Sandanu Hewage <sandanumihisara06@gmail.com>
2024-08-26 18:36:23 +0530

    Third Change

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My Name<code>src/App.js</code> is Sandanu Hewage.
-        </p>
+        <p>My Name is Sandanu Hewage I am currently working on this site.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
